7aa6eeb17e5659b08b6c682269f9e4a22df6d490
eswar-sai-123 <pbeswarsai@gmail.com>
2024-08-01 12:02:33 +0530

    commit

--- hello/config.py
+++ hello/config.py
@@ -1,10 +1,12 @@
 import os 
 from dotenv import load_dotenv
+import psycopg2
 load_dotenv()
 class Config:
-    SECRET_KEY = "rammasiw"
-    SECURITY_PASSWORD_SALT = "123243353fd"
-    SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT')
+    SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+    #postgresql://blog_postgresql_blogy_user:7WdM3m8fkGkZaIil02WgWkHAjSikufjO@dpg-cqlh9tbv2p9s73areeog-a.oregon-postgres.render.com/blog_postgresql_blogy
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
